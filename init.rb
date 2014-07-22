d79447ad10ae02888e617dc0dedebb78115a739e
Marcus Rückert <mrueckert@suse.de>
2014-07-22 15:29:37 +0200

    https://github.com/a-ono/redmine_oauth_provider/commit/d7f9aae0090dd4ad9c19de9763d739f68dd88aa0
    Just pulled the init.rb change to add the admin menu.

--- init.rb
+++ init.rb
@@ -19,4 +19,6 @@ Redmine::Plugin.register :redmine_oauth_provider do
   version '0.0.1'
   url 'https://github.com/Virtualmaster/redmine-oauth-provider'
   author_url 'http://www.jana4u.net/'
+
+  menu :admin_menu, :oauth_clients, { :controller => 'oauth_clients', :action => 'index' }, :caption => :oauth_client_applications
 end
